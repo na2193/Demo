9cc20ffaab207c254719e27d1af0ef703ddf33d8
na2193 <ahmednasim28@gmail.com>
2018-07-02 16:48:18 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,9 @@
-set projectLocation=C:\Users\nasim.ahmed\Desktop\Demo
-cd %projectLocation%
-
+cd C:\Users\nasim.ahmed\Desktop\Demo\src\application
 javac TestOperations.java
 
+set projectLocation=C:\Users\nasim.ahmed\Desktop\Demo
+cd %projectLocation%
 echo Project Location = %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 echo Class Path = %classpath%
-
 java org.testng.TestNG %projectLocation%\testng.xml
