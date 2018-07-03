1b0e72c86cdefc60af9a001c3c503978ba83f43f
nasim.ahmed <nasim.ahmed@147-NAHMED.caci.com>
2018-07-03 09:34:53 -0400

    commit

--- run.bat
+++ run.bat
@@ -1,11 +1,12 @@
 cd C:\Users\nasim.ahmed\Desktop\Demo\src\application
 
 set projectLocation=C:\Users\nasim.ahmed\Desktop\Demo
-cd %projectLocation%
+
 echo Project Location = %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 echo Class Path = %classpath%
 
+cd %projectLocation%
 javac Operation.Java
 javac TestOperations.java
 
