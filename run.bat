ea6886009c26b6a857c7ff126a993dd8f3643a8f
nasim.ahmed <nasim.ahmed@147-NAHMED.caci.com>
2018-07-03 09:29:51 -0400

    configured run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,12 @@
 cd C:\Users\nasim.ahmed\Desktop\Demo\src\application
-javac TestOperations.java
 
 set projectLocation=C:\Users\nasim.ahmed\Desktop\Demo
 cd %projectLocation%
 echo Project Location = %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 echo Class Path = %classpath%
+
+javac Operation.Java
+javac TestOperations.java
+
 java org.testng.TestNG %projectLocation%\testng.xml
